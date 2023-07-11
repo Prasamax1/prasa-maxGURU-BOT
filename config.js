e25057eba2b21e9c8c5199681cdfb3a63b044ca9
Prasamax1 <139199901+Prasamax1@users.noreply.github.com>
2023-07-11 10:56:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94755279980', 'me', true],
+  ['94755279980', 'prasamax', true], 
   [''] 
 ] //Number of owners
 
